bd51e3c56323a0281345524fd813b8cec71b2760
Dewansh Gupta <82531138+dewansh999@users.noreply.github.com>
2025-10-08 14:23:28 +0530

    Update CustomerRestController.java

--- src/main/java/in/expedia/rest/CustomerRestController.java
+++ src/main/java/in/expedia/rest/CustomerRestController.java
@@ -1,14 +1,6 @@
 package in.expedia.rest;
 
-import java.util.Arrays;
-import java.util.List;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
 
 
 import in.expedia.binding.Customer;
